7c51a00ff50eabb85ac96ff77231dec772bbb937
unknown <aarti.panchal128@gmail.com>
2021-12-24 21:30:36 +0530

    changes made to package and app js file

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-         Hello Aarti!! yea!! this is test app running...
+         Hello Aarti!! yea!! this is test app running2...
         </p>
        
       </header>
